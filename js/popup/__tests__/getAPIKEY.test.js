09440d536000145df8126d2c0f96dbe666d6c374
nitatemic <alexandre@nitatemic.dev>
2022-06-13 01:57:16 +0200

    Change a test

--- js/popup/__tests__/getAPIKEY.test.js
+++ js/popup/__tests__/getAPIKEY.test.js
@@ -5,9 +5,7 @@ const API_KEY_TEST = process.env.API_KEY_TEST;
 test(`Return the API key`, () => {
 	/* Set the API key in the input field */
 	document.body.innerHTML =
-		`<div>
-			<input id="API_KEY" type="text" value="${API_KEY_TEST}">
-		</div>`;
+		`<input id="API_KEY" type="text" value="${API_KEY_TEST}">`;
 
 	/* Check the value of the API key */
 	expect(getAPIKEY()).toBe(API_KEY_TEST);
